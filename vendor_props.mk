2824ccb946edbfd954b8649e230ce38f3b18e8ed
CANALLA <39260369+CANALLA@users.noreply.github.com>
2020-11-08 19:49:29 +0100

    Update vendor_props.mk

--- vendor_props.mk
+++ vendor_props.mk
@@ -7,5 +7,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.release_type=release \
     ro.vendor.build.real.device=true \
-    vendor.product.device=k3_Pro \
+    vendor.product.device=K3_Pro \
     vendor.product.manufacturer=OUKITEL
